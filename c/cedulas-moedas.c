5de49dc3f7c840f2fe1bb42e586d52296e675fbc
= <=>
2023-04-03 14:01:04 -0300

    lógica da atv 1021 beecrowd finalizada

--- c/cedulas-moedas.c
+++ c/cedulas-moedas.c
@@ -26,17 +26,30 @@ int main(){
     //input
     scanf("%lf", &n);
 
+
     //notas
     /*calculo buscando retirar os inteiros para quantificar as cedulas*/
-    cem = rondo(n/100); //aplicando roundo em todas os calculos se for necessario
-    cinq = rondo((fmod(cem, 1)*100)/50); //aplicando fmod para retirar os inteiros e contabilizar as cedulas
-    vinte = rondo((fmod(cinq, 1)*50/20));
-    dez = rondo((fmod(vinte, 1)*20)/10);
-    cinco = rondo((fmod(dez, 1)*10)/5);
-    dois = rondo((fmod(cinco, 1)*5)/2);
+    cem = rondo(floor(n)/100); //aplicando roundo em todas os calculos se for necessario
+    cinq = rondo(fmod(cem, 1)*100)/50; //aplicando fmod para retirar os inteiros e contabilizar as cedulas
+    vinte = rondo(fmod(cinq, 1)*50)/20;
+    dez = rondo(fmod(vinte, 1)*20)/10;
+    cinco = rondo(fmod(dez, 1)*10)/5;
+    dois = rondo(fmod(cinco, 1)*5)/2;
 
 
-    
+    //moedas
+
+    um = rondo((fmod(dois, 1)*2));
+/*
+multipliquei por 100 para aplicar a mesma formula
+das notas 
+*/
+    moedas = rondo(fmod(n, 1))*100; 
+    cinq2 = rondo(moedas/50); 
+    vinte2 = rondo(fmod(cinq2, 1)*50)/25; 
+    dez2 = rondo(fmod(vinte2, 1)*25)/10;
+    cinco2 = rondo(fmod(dez2, 1)*10)/5;
+    um2 = rondo(fmod(cinco2, 1))*5;
 
 
 
@@ -45,60 +58,80 @@ int main(){
     primeiro inteiro encontrado menor       
     do que o valor resultado da operação
 */
-    //output
+    //output notas
     printf("NOTAS:\n");
-    printf("%.0f nota(s) de R$ 100,00\n", floor(cem));  
-    printf("%.0f nota(s) de R$ 50,00\n", floor(cinq));
-    printf("%.0f nota(s) de R$ 20,00\n", floor(vinte));
-    printf("%.0f nota(s) de R$ 10,00\n", floor(dez));
-    printf("%.0f nota(s) de R$ 5,00\n", floor(cinco));
-    printf("%.0f nota(s) de R$ 2,00\n", floor(dois));
+    printf("%d nota(s) de R$ 100.00\n", (int)floor(cem));  
+    printf("%d nota(s) de R$ 50.00\n", (int)floor(cinq));
+    printf("%d nota(s) de R$ 20.00\n", (int)floor(vinte));
+    printf("%d nota(s) de R$ 10.00\n", (int)floor(dez));
+    printf("%d nota(s) de R$ 5.00\n", (int)floor(cinco));
+    printf("%d nota(s) de R$ 2.00\n", (int)floor(dois));
 
-    //moedas
+    //output moedas
+    printf("MOEDAS:\n");
+    printf("%d moeda(s) de R$ 1.00\n", (int)floor(um));
+    printf("%d moeda(s) de R$ 0.50\n", (int)floor(cinq2));  
+    printf("%d moeda(s) de R$ 0.25\n", (int)floor(vinte2));
+    printf("%d moeda(s) de R$ 0.10\n", (int)floor(dez2));
+    printf("%d moeda(s) de R$ 0.05\n", (int)floor(cinco2));
+    printf("%d moeda(s) de R$ 0.01\n\n", (int)floor(um2));
+
+/* solução lógica para as moedas
+
     printf("MOEDAS:\n");
 
     um = rondo((fmod(dois, 1)*2));
-    printf("%.0f nota(s) de R$ 1,00\n", floor(um));
+    printf("%.0f moedas(s) de R$ 1.00\n", floor(um));
 
     moedas = rondo(fmod(n, 1));
 
-    if ((moedas/0.5)>0){
+    if ((moedas/0.5)>=1){
         cinq2 = moedas/0.5;
-        printf("%.0f nota(s) de R$ 0,5\n", floor(cinq2));
+        printf("%.0f moedas(s) de R$ 0.50\n", floor(cinq2));
         
         moedas = moedas-0.5;
 
-        if((moedas/0.25)>0){
+        } else{
+        printf("0 moedas(s) de R$ 0.50\n");
+        }
+        
+
+
+        if((moedas/0.25)>=1){
             vinte2 = moedas/0.25;
-            printf("%.0f nota(s) de R$ 0,25\n", floor(vinte2));     
+            printf("%.0f moedas(s) de R$ 0.25\n", floor(vinte2));     
             moedas = (moedas-0.25);
         }else{
+            printf("0 moedas(s) de R$ 0.25\n");
+        } 
 
-        } printf("0 moedas(s) de R$ 0,25\n");
 
-        if((moedas/0.1)>0){
+        
+        if((moedas/0.1)>=1){
             dez2 = moedas/0.1;
-            printf("%.0f nota(s) de R$ 0,25\n", floor(dez2));
+            printf("%.0f moedas(s) de R$ 0.10\n", floor(dez2));
             
             moedas = (moedas-((floor(dez2))*0.1));
 
         } else{
-            printf("0 nota(s) de R$ 0,25\n");
-
+            printf("0 moedas(s) de R$ 0.10\n");
         }
 
-        if((moedas/0.01)>0){
-            um2 = moedas/0.01;
-            printf("%.0f nota(s) de R$ 0,25\n", floor(um2));   
+        if((moedas/0.05)>=1){
+            um2 = moedas/0.05;
+            printf("%.0f moedas(s) de R$ 0.05\n", floor(um2));   
         } else{
-            printf("0 nota(s) de R$ 0,25\n");   
-
+            printf("0 moedas(s) de R$ 0.05\n");   
         }
 
+        if((moedas/0.01)>=1){
+            um2 = moedas/0.01;
+            printf("%.0f moedas(s) de R$ 0.01\n", floor(um2));   
+        } else{
+            printf("0 moedas(s) de R$ 0.01\n");   
+        }
 
-    } else{
-        printf("0 moedas(s) de R$ 0,50\n");
-    }
+*/
 
     return 0;
 
