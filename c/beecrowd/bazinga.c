90bb062a78a8c3e3e1d133bdc0367665e5031d2e
= <=>
2023-05-15 11:01:28 -0300

    atv 1828 beecrowd finalizada

--- c/beecrowd/bazinga.c
+++ c/beecrowd/bazinga.c
@@ -4,51 +4,73 @@ data: ;
 atv 1828
 */
 
-/*
-lagarto >Spock/paper
-pedra   >tesoura/lagarto
-papel   >pedra/Spock
-Spock   >tesoura/pedra
-tesoura >papel/lagarto
-*/
 #include<stdio.h>
 #include<string.h>
 
-int main()
-    
+char jogada (char string);
+
+
+
+int main ()
 {
-    char jogada[5][8]={"pedra","papel","tesoura","lagarto","Spock"}
-    char str1[8],str2[8];
-    fgets(str1, 8, stdin);
-    fgets(str2, 8, stdin);
-   
+    int comp, x, y=1;   //controladores
+    char opcoes[5][8]={"pedra","papel","tesoura","lagarto","Spock"};
+    char string1[8];  //sheldon
+    char string2[8];    //raj
+    char sheldon[8], raj[8];
 
+    scanf("%d", &x);
 
-    if (strcmp(str1,str2)==0)
-        printf("Caso #x: Bazinga!\n");
-    
-    if (str1==resultado)
+    while (y<=x)
+    {
+        scanf(" %[^ ]s", string1);
+        scanf(" %[^\n]s", string2);
 
+        for (int i = 0; i < 6; i++)     //jogada do sheldon
+        {
+            if (strcmp(string1, opcoes[i])==0)
+            {
+                strcpy(sheldon, string1);
+                break;
+            }
+        }
+        
+        for (int i = 0; i < 6; i++)     //jogada do raj
+        {
+            comp=strcmp(string2, opcoes[i]);
+
+            if (comp==0)
+            {
+                strcpy(raj, string2);
+                break;
+            }
+        }
+/*
+    0 - pedra   >tesoura/lagarto    0 - 2/3
+    1 - papel   >pedra/Spock        1 - 0/4
+    2 - tesoura >papel/lagarto      2 - 1/3
+    3 - lagarto >Spock/paper        3 - 4/1
+    4 - Spock   >tesoura/pedra      4 - 2/0
+*/
+        if(strcmp(string1,string2)==0)  //empate
+        {
+            printf("Caso #%d: De novo!\n",y);
+            y++;
+            continue;
+        }
+        if (strcmp(sheldon,opcoes[0])==0)
+            (strcmp(sheldon,opcoes[0])==0&&strcmp(raj,opcoes[2])==0||strcmp(raj,opcoes[3])==0) ?  printf("Caso #%d: Bazinga!\n",y) : printf("Caso #%d: Raj trapaceou!\n",y);
+        if(strcmp(sheldon,opcoes[1])==0)
+            (strcmp(sheldon,opcoes[1])==0&&strcmp(raj,opcoes[0])==0||strcmp(raj,opcoes[4])==0) ?  printf("Caso #%d: Bazinga!\n",y) : printf("Caso #%d: Raj trapaceou!\n",y);
+        if(strcmp(sheldon,opcoes[2])==0)
+            (strcmp(sheldon,opcoes[2])==0&&strcmp(raj,opcoes[1])==0||strcmp(raj,opcoes[3])==0) ?  printf("Caso #%d: Bazinga!\n",y) : printf("Caso #%d: Raj trapaceou!\n",y);
+        if(strcmp(sheldon,opcoes[3])==0)
+            (strcmp(sheldon,opcoes[3])==0&&strcmp(raj,opcoes[4])==0||strcmp(raj,opcoes[1])==0) ?  printf("Caso #%d: Bazinga!\n",y) : printf("Caso #%d: Raj trapaceou!\n",y);
+        if(strcmp(sheldon,opcoes[4])==0)
+            (strcmp(sheldon,opcoes[4])==0&&strcmp(raj,opcoes[2])==0||strcmp(raj,opcoes[0])==0) ?  printf("Caso #%d: Bazinga!\n",y) : printf("Caso #%d: Raj trapaceou!\n",y);
+
+        y++;
+    }
     return 0;
 }
 
-int resultado (char string[8])
-{
-    char jogada[5][8]={"pedra","papel","tesoura","lagarto","Spock"}
-
-
-    for (i = 0; i < 6; i++)
-    {
-        for (j = 0; j < 8; j++)
-        {
-        strcmp(string, jogada[i][j])
-        if (strcmp==0)
-            break;
-        
-        }
-        
-    }
-    
-
-return x;
-}
--- c/beecrowd/teste.c
+++ c/beecrowd/teste.c
@@ -7,81 +7,19 @@ char jogada (char string);
 
 int main ()
 {
-    int comp;
-
-        /*
-    0 - pedra
-    1 - papel
-    2 - tesoura
-    3 - lagarto
-    4 - Spock
-    */
+    int comp, x, y=1;   //controladores
     char opcoes[5][8]={"pedra","papel","tesoura","lagarto","Spock"};
-    char string1[]= "tesoura";  //sheldon
-    char string2[]= "papel";
+    char string1[8];  //sheldon
+    char string2[8];    //raj
     char sheldon[8], raj[8];
     //char sheldon[8]=jogada(string1), raj[8]=jogada(string2);
 
+    scanf("%d", &x);
 
-    for (int i = 0; i < 6; i++)     //jogada do sheldon
+    while (y<=x)
     {
-        if (strcmp(string1, opcoes[i])==0)
-        {
-            strcpy(sheldon, string1);
-            break;
-        }
-    }
-    
-    for (int i = 0; i < 6; i++)     //jogada do raj
-    {
-        comp=strcmp(string2, opcoes[i]);
-
-        if (comp==0)
-        {
-            strcpy(raj, string2);
-            break;
-        }
-    }
-
-    printf("\n\n%s\n",sheldon);
-    printf("%s\n",raj);
-    if (strcmp(sheldon,opcoes[0])==0)
-        (strcmp(sheldon,opcoes[0])==0&&strcmp(raj,opcoes[2])==0||strcmp(raj,opcoes[3])==0) ?  printf("SHELDON GANHA\n") : printf("SHELDON PERDEU\n");
-    if(strcmp(sheldon,opcoes[1])==0)
-        (strcmp(sheldon,opcoes[1])==0&&strcmp(raj,opcoes[0])==0||strcmp(raj,opcoes[4])==0) ?  printf("SHELDON GANHA\n") : printf("SHELDON PERDEU\n");
-    if(strcmp(sheldon,opcoes[2])==0)
-        (strcmp(sheldon,opcoes[2])==0&&strcmp(raj,opcoes[1])==0||strcmp(raj,opcoes[3])==0) ?  printf("SHELDON GANHA\n") : printf("SHELDON PERDEU\n");
-    if(strcmp(sheldon,opcoes[3])==0)
-        (strcmp(sheldon,opcoes[3])==0&&strcmp(raj,opcoes[4])==0||strcmp(raj,opcoes[1])==0) ?  printf("SHELDON GANHA\n") : printf("SHELDON PERDEU\n");
-    if(strcmp(sheldon,opcoes[4])==0)
-        (strcmp(sheldon,opcoes[4])==0&&strcmp(raj,opcoes[2])==0||strcmp(raj,opcoes[0])==0) ?  printf("SHELDON GANHA\n") : printf("SHELDON PERDEU\n");
-}
-
-/*
-    pedra   >tesoura/lagarto
-    papel   >pedra/Spock
-    tesoura >papel/lagarto
-    lagarto >Spock/paper
-    Spock   >tesoura/pedra
-
-
-    0 - 2/3
-    1 - 0/4
-    2 - 1/3
-    3 - 4/1
-    4 - 2/0
-
-    0 - pedra
-    1 - papel
-    2 - tesoura
-    3 - lagarto
-    4 - Spock
-    */
-
-   
-   /*
-    char jogada (char string1[8], char sheldon[8])
-    {
+        scanf(" %[^ ]s", string1);
+        scanf(" %[^\n]s", string2);
 
         for (int i = 0; i < 6; i++)     //jogada do sheldon
         {
@@ -91,6 +29,50 @@ int main ()
                 break;
             }
         }
-        return  sheldon;
+        
+        for (int i = 0; i < 6; i++)     //jogada do raj
+        {
+            comp=strcmp(string2, opcoes[i]);
+
+            if (comp==0)
+            {
+                strcpy(raj, string2);
+                break;
+            }
+        }
+        if(strcmp(string1,string2)==0)
+        {
+            printf("Caso #%d: De novo!\n",y);
+            y++;
+            continue;
+        }
+        if (strcmp(sheldon,opcoes[0])==0)
+            (strcmp(sheldon,opcoes[0])==0&&strcmp(raj,opcoes[2])==0||strcmp(raj,opcoes[3])==0) ?  printf("Caso #%d: Bazinga!\n",y) : printf("Caso #%d: Raj trapaceou!\n",y);
+        if(strcmp(sheldon,opcoes[1])==0)
+            (strcmp(sheldon,opcoes[1])==0&&strcmp(raj,opcoes[0])==0||strcmp(raj,opcoes[4])==0) ?  printf("Caso #%d: Bazinga!\n",y) : printf("Caso #%d: Raj trapaceou!\n",y);
+        if(strcmp(sheldon,opcoes[2])==0)
+            (strcmp(sheldon,opcoes[2])==0&&strcmp(raj,opcoes[1])==0||strcmp(raj,opcoes[3])==0) ?  printf("Caso #%d: Bazinga!\n",y) : printf("Caso #%d: Raj trapaceou!\n",y);
+        if(strcmp(sheldon,opcoes[3])==0)
+            (strcmp(sheldon,opcoes[3])==0&&strcmp(raj,opcoes[4])==0||strcmp(raj,opcoes[1])==0) ?  printf("Caso #%d: Bazinga!\n",y) : printf("Caso #%d: Raj trapaceou!\n",y);
+        if(strcmp(sheldon,opcoes[4])==0)
+            (strcmp(sheldon,opcoes[4])==0&&strcmp(raj,opcoes[2])==0||strcmp(raj,opcoes[0])==0) ?  printf("Caso #%d: Bazinga!\n",y) : printf("Caso #%d: Raj trapaceou!\n",y);
+
+        y++;
     }
-    */
+    return 0;
+}
+
+/*
+    0 - pedra   >tesoura/lagarto
+    1 - papel   >pedra/Spock
+    2 - tesoura >papel/lagarto
+    3 - lagarto >Spock/paper
+    4 - Spock   >tesoura/pedra
+
+
+    0 - 2/3
+    1 - 0/4
+    2 - 1/3
+    3 - 4/1
+    4 - 2/0
+*/
