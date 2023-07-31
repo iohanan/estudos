43d46837113d8fb87f29fb3cd14f1f0b32d9f016
= <=>
2023-07-31 14:34:34 -0300

    CFBCursos aulas 12, 13 finalizadas

---
+++ python/CFBCursos/Aula12/aula12.py
@@ -0,0 +1,8 @@
+carros=["HRV","Golf", "Argo","Focus"]
+
+for x in carros:
+    print(x)
+    if(x=="Arg"):
+        break
+
+print("Fim do programa")
---
+++ python/CFBCursos/Aula13/aula13.py
@@ -0,0 +1,15 @@
+
+#limpar a tela
+import os
+os.system('cls')
+
+
+nome=input("Digite seu nome: ")
+
+print("Curso de Python\nUsuário: "+nome)
+num1=int(input("Digite o primeiro valor: "))
+num2=int(input("Digite o segundo valor: "))
+
+res=num1+num2
+
+print("Soma dos valores: "+str(res))
