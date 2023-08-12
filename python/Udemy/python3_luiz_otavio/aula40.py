27ec2b80677a194e66175da22569b97cdc8e14c1
iohanan <iohanan.monteiro@gmail.com>
2023-08-12 18:38:22 -0400

    python/Udemy/python3_luiz_otavio/aula40.py finalizada

--- python/Udemy/python3_luiz_otavio/aula40.py
+++ python/Udemy/python3_luiz_otavio/aula40.py
@@ -1,22 +1,57 @@
 '''Calculadora com while'''
 
 while True:
-    num_1=input('Digite o primeiro valor: ')
-    num_2=input('Digite o segundo valor: ')
-    
-    opcao=input(
-        '\n\n\
-        1 - Soma\n\
-        2 - Subtração\n\
-        3 - Multiplicação\n\
-        4 - Divisão\n\n\
-        0 - Sair\n\n\
-        Selecione a opção: '
-        )
-    if opcao=='0':  #SAIR
-        break
-    elif opcao=='1':    #SOMA
-        soma=num_1+num_2
-        print('Soma = ', soma)
+    #VARIAVEIS DE CONTROLE DE VALIDAÇÃO
+    num_valido=None
+    operadores=('+-*/')
+
+    while num_valido is None:   #CONTROLE DE VALIDAÇÃO
+        try:
+            num_1=float(input('\nDigite o primeiro valor: '))
+            num_2=float(input('Digite o segundo valor: '))
+            num_valido=True
+            break
+        except:
+            num_valido=None
         
+        if num_valido is None:
+            print('ENTRADA INVÁLIDA TENTE NOVAMENTE\n')
+            continue
+
+    opcao=input(
+'\n\n\
++ - Soma\n\
+- - Subtração\n\
+* - Multiplicação\n\
+/ - Divisão\n\n\
+Selecione a opção: '
+        )
+
+    if len(opcao)>1:    #CONTROLE DE VALIDAÇÃO
+        print('Digite apenas um operador.')
+        continue 
+    elif opcao not in operadores: #CONTROLE DE VALIDAÇÃO
+        print('Operador Inválido.')
+        continue
+
+
+    elif opcao=='+':    #SOMA
+        soma=num_1+num_2
+        print(f'\n\nSoma = {soma}\n')
+
+    elif opcao=='-':    #SUBTRAÇÃO
+        subtracao=num_1-num_2
+        print(f'\n\nSubtração = {subtracao}\n')
+
+    elif opcao=='*':    #MULTIPLICAÇÃO
+        mult=num_1*num_2
+        print(f'\n\nMultiplicação = {mult}\n')
+
+    elif opcao=='/':    #DIVISÃO
+        div=num_1/num_2
+        print(f'\n\nDivisão = {div}\n')
+        
+    inout = input('Você quer [s]air? ').lower().startswith('s') #SAIR DO LOOP
+    if inout is True:
+        break
         
