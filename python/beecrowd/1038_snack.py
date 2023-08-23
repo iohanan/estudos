f44840f416c811796d4bd0740a6c695d51e9813b
unknown <iohanan.monteiro@gmail.com>
2023-08-23 12:29:51 -0400

    atividade atualizada

--- python/beecrowd/1038_snack.py
+++ python/beecrowd/1038_snack.py
@@ -1,20 +1,9 @@
-class Produto:
-    def __init__(self,c,n,p):
-        self.code=c
-        self.nome=n
-        self.price=p
-
-snack1 = Produto(1,'Cachorro Quente', 4.0)
-snack2 = Produto(2,'X-Salada', 4.5)
-snack3 = Produto(3,'X-Baacon', 5.0)
-snack4 = Produto(4,'Torrada simples', 2.0)
-snack5 = Produto(5,'Refrigerante', 1.5)
-snacks = 4.0, 4.5, 5.0, 2.0, 1.5
-#         0    1    2    3    4            
+snacks_prices = 4.0, 4.5, 5.0, 2.0, 1.5
+#                0    1    2    3    4            
 codigo,qtd = [int(x) for x in input().split()]
 count=1
-for x in snacks:
+for item in snacks_prices:
     if codigo == count:
-        total=x*qtd
+        total=item*qtd
         print(f'Total: R$ {total:.2f}')
     count+=1
