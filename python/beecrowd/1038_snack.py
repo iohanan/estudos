b173f3077a26d2b13248a464868f70e0705fa5d0
unknown <iohanan.monteiro@gmail.com>
2023-08-23 10:55:05 -0400

    python/beecrowd/1038_snack.py finalizado

--- python/beecrowd/1038_snack.py
+++ python/beecrowd/1038_snack.py
@@ -9,5 +9,12 @@ snack2 = Produto(2,'X-Salada', 4.5)
 snack3 = Produto(3,'X-Baacon', 5.0)
 snack4 = Produto(4,'Torrada simples', 2.0)
 snack5 = Produto(5,'Refrigerante', 1.5)
-
+snacks = 4.0, 4.5, 5.0, 2.0, 1.5
+#         0    1    2    3    4            
 codigo,qtd = [int(x) for x in input().split()]
+count=1
+for x in snacks:
+    if codigo == count:
+        total=x*qtd
+        print(f'Total: R${total:.2f}')
+    count+=1
