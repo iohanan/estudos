749ace3cbd0c600b3a81153d62a78a81969ae40c
unknown <iohanan.monteiro@gmail.com>
2023-08-24 13:12:14 -0400

    python/beecrowd/ atv 1042 logica em desenvolvimento

---
+++ python/beecrowd/1042_simple_sort.py
@@ -0,0 +1,15 @@
+a, b,c = [int(x) for x in input().split()]
+original = [a,b,c]
+sequencia = list(original)
+print('Original:',original)
+print('Seque:',sequencia)
+
+
+
+for item in sequencia:
+    print(item)
+
+print('')
+
+for item in original:
+    print(item)
