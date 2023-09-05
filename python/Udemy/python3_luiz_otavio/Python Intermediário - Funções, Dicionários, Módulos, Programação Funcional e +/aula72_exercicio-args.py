4489b4dd845ccd3f0e75a5c43f9c87ed91a8c9be
unknown <iohanan.monteiro@gmail.com>
2023-09-05 14:06:19 -0400

    python/Udemy/python3_luiz_otavio/ aulas 72,73 finalizada

--- python/Udemy/python3_luiz_otavio/Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/aula72_exercicio-args.py
+++ python/Udemy/python3_luiz_otavio/Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/aula72_exercicio-args.py
@@ -3,21 +3,20 @@ os.system('cls')
 
 def multip(*args):
     if bool(args) is False:
-        return print(0)
+        return 0
     total = 1
     for num in args:
         total*=num
-    return print(total)
+    return total
     
 def parouimpar(x):
     valor = x%2==0
     if valor:
-        print('É par.')
-    else:
-        print('É impar.')
+        return f'{x} é par.'
+    return f'{x} é ímpar.'
 
-parouimpar(9)
-parouimpar(8)
+print(parouimpar(9))
+print(parouimpar(8))
 print(4*5*1*2*3*7)
-multip(4,5,1,2,3,7)
-multip()
+m = multip(4,5,1,2,3,7)
+print(m)
