cb63ff163c4691dcc27385128ec07dcf7604c333
Iohanan M <iohanan.monteiro@gmail.com>
2023-09-06 12:17:18 -0400

    aula 76 em andamento

---
+++ python/Udemy/python3_luiz_otavio/Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/aula76.py
@@ -0,0 +1,44 @@
+# Dicionários em Python (tipo dict)
+# Dicionários são estruturas de dados do tipo
+# par de "chave" e "valor".
+# Chaves podem ser consideradas como o "índice"
+# que vimos na lista e podem ser de tipos imutáveis
+# como: str, int, float, bool, tuple, etc.
+# O valor pode ser de qualquer tipo, incluindo outro
+# dicionário.
+# Usamos as chaves - {} - ou a classe dict para criar
+# dicionários.
+# Imutáveis: str, int, float, bool, tuple
+# Mutável: dict, list
+# pessoa = {
+#     'nome': 'Luiz Otávio',
+#     'sobrenome': 'Miranda',
+#     'idade': 18,
+#     'altura': 1.8,
+#     'endereços': [
+#         {'rua': 'tal tal', 'número': 123},
+#         {'rua': 'outra rua', 'número': 321},
+#     ]
+# }
+# pessoa = dict(nome='Luiz Otávio', sobrenome='Miranda')
+
+humano = {
+    'nome': 'Luiz Otávio',
+    'sobrenome': 'Miranda',
+    'idade': 18,
+    'altura': 1.72,
+    'enderecos': [
+        {'rua': 'tal tal', 'numero': 123},
+        {'rua': 'tal tal', 'numero': 123},
+    ]
+}
+
+print(type(humano))
+print(humano['nome'])
+print()
+for chave in humano:
+    print(f'{chave} : {humano[chave]}')
+
+#####   Manipulando chaves e valores em dicionários 
+
+ 
