a9b0e7240cb33407d60161e8153f967c75c53230
Iohanan M <iohanan.monteiro@gmail.com>
2023-10-02 09:47:01 -0400

    laboratórios adicionados

--- python/Udemy/python3_luiz_otavio/Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/aula78.py
+++ python/Udemy/python3_luiz_otavio/Python Intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/aula78.py
@@ -7,27 +7,35 @@
 
 # Criando um set
 # set(iterável) ou {1, 2, 3}
-# s1 = set('Luiz')
-# s1 = set()  # vazio
-# s1 = {'Luiz', 1, 2, 3}  # com dados
-
-# Sets são eficientes para remover valores duplicados
-# de iteráveis.
+s1 = set() #vazio
+s2 = {'Luiz',1,2,3} #com dados
+print(s1, type(s1))
+print(s2, type(s1),end='\n\n')
+# Sets são eficientes para remover valores duplicados de iteráveis.
 # - Seus valores serão sempre únicos;
 # - Não aceitam valores mutáveis;
 # - não tem índexes;
 # - não garantem ordem;
 # - são iteráveis (for, in, not in)
-# l1 = [1, 2, 3, 3, 3, 3, 3, 1]
-# s1 = set(l1)
-# l2 = list(s1)
-# s1 = {1, 2, 3}
-# print(3 not in s1)
-# for numero in s1:
-#     print(numero)
+set1 = {1,2,2,4,4,4,2,2,3,3,3,3,3,'teste'}
+print(f'remoção de valores duplicados\n{set1} {type(set1)}')
+print(3 in set1)
+for i in set1:
+    print(i)
+print('\n\n')
 
 # Métodos úteis:
 # add, update, clear, discard
+lista = set()
+lista.add(1)
+lista.update([2])
+lista.update((2,3,4,'teste','nomes',4545))
+
+lista.discard('nomes')
+lista.discard(4545)
+
+print(lista, type(lista))
+
 
 # Operadores úteis:
 # união | união (union) - Une
